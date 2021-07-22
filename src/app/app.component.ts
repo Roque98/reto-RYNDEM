3bd0dc6f2aba719ad0a14bd49b94fecb09f56ae6
Roque Ayala Angel David <ingRoque98@gmail.com>
2021-07-22 14:59:15 -0500

    final del reto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,97 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { generateMongoObjectId } from './core/utils/mongoIdGenerator';
+import { Book } from './models/book.model';
+import { BooksService } from './services/books.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'frontendLibros';
+export class AppComponent implements OnInit {
+  books: Book[] = [];
+  bookUpdate: Book | undefined;
+  updatingBook = false;
+
+  constructor(private booksService: BooksService) {}
+
+  ngOnInit() {
+    this.getAllBooks();
+  }
+
+  bookForm = new FormGroup({
+    title: new FormControl('', Validators.required),
+    author: new FormControl('', Validators.required),
+  });
+
+  isInvalidTitle() {
+    return (
+      this.bookForm.get('title')?.invalid && this.bookForm.get('title')?.touched
+    );
+  }
+
+  isInvalidAuthor() {
+    return (
+      this.bookForm.get('author')?.invalid &&
+      this.bookForm.get('author')?.touched
+    );
+  }
+
+  getAllBooks() {
+    this.booksService.getAllBooks().subscribe((books) => {
+      this.books = books;
+    });
+  }
+
+
+  createBook() {
+    this.bookForm.markAllAsTouched();
+    if (this.bookForm.valid) {
+      const newBook: Book = {
+        id: generateMongoObjectId(),
+        title: this.bookForm.get('title')?.value,
+        author: this.bookForm.get('author')?.value,
+      };
+
+      this.booksService.createBook(newBook).subscribe((book) => {
+        this.getAllBooks();
+        this.bookForm.reset();
+      });
+    }
+  }
+
+  activeEditBook(book: Book){
+    this.bookUpdate = book;
+    this.updatingBook = true;
+    this.bookForm.patchValue(book);
+  }
+
+  editBook() {
+    this.bookForm.markAllAsTouched();
+    if (this.bookForm.valid) {
+      const newBook: Book = {
+        id: this.bookUpdate?.id || '',
+        title: this.bookForm.get('title')?.value,
+        author: this.bookForm.get('author')?.value,
+      };
+
+      this.booksService.updateBook(newBook).subscribe((books) => {
+        this.getAllBooks();
+        this.bookForm.reset();
+        this.updatingBook = false;
+      });
+    }
+  }
+
+  cancelUpdate(){
+    this.updatingBook = false;
+    this.bookForm.reset();
+  }
+
+  deleteBook(book: Book) {
+    this.booksService.deleteBook(book.id).subscribe((book) => {
+      this.getAllBooks();
+    });
+  }
 }
---
+++ src/app/services/books.service.ts
@@ -0,0 +1,32 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Book } from '../models/book.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BooksService {
+
+  url = "http://192.168.0.3:8080/api"
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  getAllBooks(){
+    return this.http.get<Book[]>(`${this.url}/getAllBooks`)
+  }
+
+  
+  createBook(book: Book){
+    return this.http.post<Book>(`${this.url}/createBook`, book)
+  }
+
+  updateBook(book: Book){
+    return this.http.put<Book[]>(`${this.url}/updateBook`, book);
+  }
+
+  deleteBook(id: string){
+    return this.http.delete(`${this.url}/deleteBook/${id}`);
+  }
+}
